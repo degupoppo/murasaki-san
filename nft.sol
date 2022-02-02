2f533fd5cbba830dee8dfcea9f4a74fe3917c7ac
keijoppo <keijo_a@yahoo.co.jp>
2022-02-02 11:42:38 +0900

    @

--- nft.sol
+++ nft.sol
@@ -556,7 +556,101 @@ abstract contract ERC721Enumerable is ERC721, IERC721Enumerable {
     }
 }
 
-contract murasaki is ERC721{
+
+//from rarity_skin
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+//from rarity_skin
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+    RandomDataGuard internal _rdg;
+    string internal _planetsBaseURI;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _setOwner(_msgSender());
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+    
+    function setRDG(address rdg)public virtual onlyOwner {
+        _rdg = RandomDataGuard(rdg);    
+    }
+
+    function setBaseURI(string memory pBaseURI)public virtual onlyOwner {
+        _planetsBaseURI = pBaseURI;    
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _setOwner(newOwner);
+    }
+
+    function _setOwner(address newOwner) private {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+interface RandomDataGuard {
+    function randomGuard(uint256 random) external view returns(uint256);
+}
+
+
+
+contract murasaki is ERC721, Ownable{
 
     uint public next_summoner;
 
@@ -585,51 +679,16 @@ contract murasaki is ERC721{
     mapping(uint => uint) public next_exp_required;
 
     uint tmp;
+    uint public base = 86400;
+    uint public price = 1 ether;
 
-    /***
-    function get_status(uint _summoner) external view returns (
-            uint _ctype,
-            uint _strength,
-            uint _dexterity,
-            uint _vitality,
-            uint _intelligence,
-            uint _luck,
-            uint _birth_time,
-            uint _last_feeding_time,
-            uint _last_grooming_time,
-            uint _coin,
-            uint _material,
-            uint _mining_status,
-            uint _mining_start_time,
-            uint _farming_status,
-            uint _farming_start_time,
-            uint _crafting_status,
-            uint _crafting_start_time,
-            uint _exp,
-            uint _level,
-            uint _next_exp_required) {
-        _ctype = ctype[_summoner];
-        _strength = strength[_summoner];
-        _dexterity = dexterity[_summoner];
-        _vitality = vitality[_summoner];
-        _intelligence = intelligence[_summoner];
-        _luck = luck[_summoner];
-        _birth_time = birth_time[_summoner];
-        _last_feeding_time = last_feeding_time[_summoner];
-        _last_grooming_time = last_grooming_time[_summoner];
-        _coin = coin[_summoner];
-        _material = material[_summoner];
-        _mining_status = mining_status[_summoner];
-        _mining_start_time = mining_start_time[_summoner];
-        _farming_status = farming_status[_summoner];
-        _farming_start_time = farming_start_time[_summoner];
-        _crafting_status = crafting_status[_summoner];
-        _crafting_start_time = crafting_start_time[_summoner];
-        _exp = exp[_summoner];
-        _level = level[_summoner];
-        _next_exp_required = next_exp_required[_summoner];
+    function set_base(uint _base) public onlyOwner {
+        base = _base;
     }
-    ***/
+
+    function set_price(uint _price) public onlyOwner {
+        price = _price;
+    }
 
     function get_status(uint _summoner) public view returns (uint[20] memory) {
         uint[20] memory li_status;
@@ -657,6 +716,7 @@ contract murasaki is ERC721{
     }
 
     function summon(uint _ctype) external payable {
+        require(msg.value >= price, "price error");
         require(0 <= _ctype && _ctype <= 1);
         uint _next_summoner = next_summoner;
         ctype[_next_summoner] = _ctype;
